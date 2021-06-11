aaa97968cd3fe66f63fb7afa6d69ef9086d06ff1
Frederike Bründermann <82148810+frederikebruendermann@users.noreply.github.com>
2021-06-11 15:06:49 +0200

    Add Button test

--- src/components/Button.spec.js
+++ src/components/Button.spec.js
@@ -1,11 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import Button from "./Button";
 
-    import {render, screen} from '@testing-library/react'
-    import Button from './Button'
-      
-    describe('Button', () => {
-        it('renders', () => {
-            render(<Button />)
-            expect(screen.getByText('Button')).toBeInTheDocument()
-        })
-    })
-          
+describe("Button", () => {
+  it("has text and shows the way back", () => {
+    render(<Button children="Back" />);
+    expect(screen.getByRole("button")).toBeInTheDocument();
+  });
+});
