943742ae8dace0f7bd0bccb295e61203b7502199
Frederike Bründermann <82148810+frederikebruendermann@users.noreply.github.com>
2021-06-24 23:35:19 +0200

    Add tests to new component NavBar and new HomePage

--- src/components/NavBar.spec.js
+++ src/components/NavBar.spec.js
@@ -1,11 +1,10 @@
+import { render } from '@testing-library/react'
+import NavBar from './NavBar'
 
-    import {render, screen} from '@testing-library/react'
-    import NavBar from './NavBar'
-      
-    describe('NavBar', () => {
-        it('renders', () => {
-            render(<NavBar />)
-            expect(screen.getByText('NavBar')).toBeInTheDocument()
-        })
-    })
-          
+describe('it calls onClickSearch when clicking on the navbar', () => {
+  it('renders', () => {
+    const onClickSearch = jest.fn()
+
+    render(<NavBar onClick={onClickSearch} />)
+  })
+})
